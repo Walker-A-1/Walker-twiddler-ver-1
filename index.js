2662c198413adbdd864e5dd3df7536f2c5fa213a
Walker Argao <argao620@gmail.com>
2024-02-28 15:16:27 -0600

    "Tweet now displays when it was created"

--- index.js
+++ index.js
@@ -13,8 +13,38 @@ $(document).ready(() => {
 
     const $tweets = streams.home.slice(0).reverse().map((tweet) => {
       const $tweet = $('<div></div>');
-      const text = `@${tweet.user}: ${tweet.message}`;
-
+      //make a function to find out how long ago the tweet was created
+      let timeAgo = (timeMade) => {
+        //take in `Wed Feb 28 2024 13:33:09 GMT-0600 (Central Standard Time)` string
+        //return either
+        //`a few seconds ago`
+        //or `x minutes ago`
+        //where x is the amount of minutes it has been since that time
+        timeMade = timeMade.getTime();//time tweet created in milliseconds
+        //the difference between now and timeMade. all in milliseconds
+        let timeSince = +new Date - timeMade //time since created. in milliseconds
+        //convert timeSince into seconds
+        timeSince = Math.floor(timeSince / 1000);
+        //check if the timeSince is bigger than 59
+        if(timeSince > 59) {
+          //true than convert seconds into minutes
+          //and return `x minutes ago`
+          timeSince = Math.floor(timeSince / 60);
+          //change `minute` to `minutes` accordingly
+          if(timeSince === 1) {
+            return timeSince + ' minute ago';
+          } else {
+            return timeSince + ' minutes ago';
+          }
+        } else {
+        //false than return
+          //`x seconds ago`
+          return timeSince + ' seconds ago';
+        }
+      }
+      //add the ability to display how long ago the tweets were made
+      const text = `@${tweet.user}: ${tweet.message}. Tweet created ${timeAgo(tweet.created_at)}.`;
+      //
       $tweet.text(text);
 
       return $tweet;
